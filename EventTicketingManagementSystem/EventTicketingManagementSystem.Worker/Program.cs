ebdf5d03077e06c63c2f3c0c9ec1838f044c3866
NgoHuy21299 <duvanbeeg@gmail.com>
2025-02-19 16:03:26 +0700

    feat: create background worker and modify Dockerfile (#38)
    Co-authored-by: Huy <quanghuy21299@gmail.com>

---
+++ EventTicketingManagementSystem/EventTicketingManagementSystem.Worker/Program.cs
@@ -0,0 +1,49 @@
+
+using EventTicketingManagementSystem.Services;
+using EventTicketingManagementSystem.Data;
+using EventTicketingManagementSystem.Worker.BackgroundServices;
+
+namespace EventTicketingManagementSystem.Worker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Configuration.AddUserSecrets<Program>()
+                     .AddEnvironmentVariables();
+
+            var connectionString = builder.Configuration["ConnectionString"] ?? throw new Exception("Cannot get Connection String");
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddHttpContextAccessor();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddServiceLayer();
+            builder.Services.AddDataServices(connectionString);
+
+            // Register UserBackgroundService
+            builder.Services.AddHostedService<BookingTaskService>();
+            builder.Services.AddHostedService<DailyTaskService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
