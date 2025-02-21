ebb994dac99baaa4b78d18c6c1574f472e328a17
Huy <quanghuy21299@gmail.com>
2025-02-22 00:28:03 +0700

    Update migrations

---
+++ EventTicketingManagementSystem/EventTicketingManagementSystem.Data/Migrations/20250221172643_RemoveColumnInBookingAndPayment.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventTicketingManagementSystem.Data.Migrations
+{
+    [ExcludeFromCodeCoverage]
+    /// <inheritdoc />
+    public partial class RemoveColumnInBookingAndPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_TransactionId",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_Status",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "RefundDate",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Bookings");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_Status",
+                table: "Payments",
+                column: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_Status",
+                table: "Payments");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefundDate",
+                table: "Payments",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Bookings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_TransactionId",
+                table: "Payments",
+                column: "TransactionId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_Status",
+                table: "Bookings",
+                column: "Status");
+        }
+    }
+}
